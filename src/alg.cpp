37e0d6b031fd7eb7f9fb653b1e69b3a2caf5a486
Egor <149178595+Maklut@users.noreply.github.com>
2026-05-27 00:40:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,1 +1,68 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2022 NNTU-CS
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
+
+#include <cstddef>
+
+struct SYM {
+  char ch;
+  int prior;
+};
+
+template<typename T>
+class TPQueue {
+ private:
+  struct Node {
+    T data;
+    Node* next;
+    explicit Node(const T& value) : data(value), next(nullptr) {}
+  };
+
+  Node* head_;
+
+ public:
+  TPQueue() : head_(nullptr) {}
+
+  ~TPQueue() {
+    while (!isEmpty()) {
+      pop();
+    }
+  }
+
+  void push(const T& item) {
+    Node* newNode = new Node(item);
+
+    if (isEmpty() || item.prior > head_->data.prior) {
+      newNode->next = head_;
+      head_ = newNode;
+      return;
+    }
+
+    Node* current = head_;
+    while (current->next != nullptr &&
+           current->next->data.prior >= item.prior) {
+      current = current->next;
+    }
+
+    newNode->next = current->next;
+    current->next = newNode;
+  }
+
+  T pop() {
+    if (isEmpty()) {
+      return T{};
+    }
+
+    Node* temp = head_;
+    T result = head_->data;
+    head_ = head_->next;
+    delete temp;
+    return result;
+  }
+
+  bool isEmpty() const {
+    return head_ == nullptr;
+  }
+};
+
+#endif

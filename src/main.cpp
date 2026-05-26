b01129ce0972390207966d9e6d254da6936fd022
Egor <149178595+Maklut@users.noreply.github.com>
2026-05-27 00:40:43 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,36 @@
 // Copyright 2022 NNTU-CS
+#include <iostream>
 #include "tpqueue.h"
 
 int main() {
-    TPQueue<SYM> pqueue;
-    pqueue.push(SYM{'a', 4});
-    pqueue.push(SYM{'b', 7});
-    SYM c1 = pqueue.pop();
-    SYM c2 = pqueue.pop();
-    return 0;
+  TPQueue<SYM> pqueue;
+
+  std::cout << "=== Testing Priority Queue ===\n\n";
+
+  pqueue.push(SYM{'a', 4});
+  std::cout << "Pushed: 'a' with priority 4\n";
+
+  pqueue.push(SYM{'b', 7});
+  std::cout << "Pushed: 'b' with priority 7\n";
+
+  pqueue.push(SYM{'c', 2});
+  std::cout << "Pushed: 'c' with priority 2\n";
+
+  pqueue.push(SYM{'d', 9});
+  std::cout << "Pushed: 'd' with priority 9\n";
+
+  pqueue.push(SYM{'e', 5});
+  std::cout << "Pushed: 'e' with priority 5\n\n";
+
+  std::cout << "Extracting elements (should be in priority order):\n";
+
+  while (!pqueue.isEmpty()) {
+    SYM item = pqueue.pop();
+    std::cout << "Popped: '" << item.ch << "' with priority "
+              << item.prior << "\n";
+  }
+
+  std::cout << "\n=== Test completed ===\n";
+
+  return 0;
 }
